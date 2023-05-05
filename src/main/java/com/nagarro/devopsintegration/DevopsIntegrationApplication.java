b44d2f5a8ce550fdbb83450bd55a3b00989901db
piyushkumar06 <piyush.kumar06@nagarro.com>
2023-05-05 14:12:09 +0530

    first commit

---
+++ src/main/java/com/nagarro/devopsintegration/DevopsIntegrationApplication.java
@@ -0,0 +1,18 @@
+package com.nagarro.devopsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsIntegrationApplication {
+
+	@GetMapping
+	public String message() {return "This is devops-integration app";}
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntegrationApplication.class, args);
+	}
+
+}
